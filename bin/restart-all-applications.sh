d985bae7132240a42293198c3420a5968351aee5
Miroslav Blasko <blcham@gmail.com>
2024-12-12 14:32:13 +0100

    Add build of safety_viz docker image

--- bin/restart-all-applications.sh
+++ bin/restart-all-applications.sh
@@ -5,13 +5,23 @@ PROJECT_ROOT_DIR=$(dirname $0)/..
 cd $PROJECT_ROOT_DIR
 
 
-restart_application() {
-docker-compose pull
-docker-compose down
-docker-compose up -d
+build_safety_viz_image() {
+  cd ../safety_viz
+  docker build . -t safety_viz
+  cd -
 }
 
 
+
+restart_application() {
+  docker-compose pull
+  docker-compose down
+  docker-compose up -d
+}
+
+
+echo "INFO: Building safety_viz image ..."
+build_safety_viz_image
 echo "INFO: Starting LKPR deployment ..."
 restart_application
 cd -
