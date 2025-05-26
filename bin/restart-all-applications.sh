bf33b9eb00d0cfe2e9fed3e3ee8eb96621aa6d99
Miroslav Blasko <blcham@gmail.com>
2025-05-26 16:45:57 +0200

    [#35] Workaround to docker compose pull command

--- bin/restart-all-applications.sh
+++ bin/restart-all-applications.sh
@@ -14,7 +14,8 @@ build_safety_viz_image() {
 
 
 restart_application() {
-  docker-compose pull
+  # workaround to docker-compose pull, it does not work due to safety-performance service
+  docker-compose pull nginx auth-server-db auth-server termit-server db-server-proxy annotace-server termit ontographer safety-performance-server s-pipes-engine db-server scipy auth-server-db auth-server termit-server annotace-server db-server
   docker-compose down
   docker-compose up -d
 }
