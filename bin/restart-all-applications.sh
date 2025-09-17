e91919af8d806a54368bf8187e4d88af84bc5182
Miroslav Blasko <blcham@gmail.com>
2025-09-17 14:53:17 +0200

    [Fix] Badly called pull command
    Instead of docker compose pull we called docker pull.

--- bin/restart-all-applications.sh
+++ bin/restart-all-applications.sh
@@ -4,25 +4,15 @@ PROJECT_ROOT_DIR=$(dirname $0)/..
 
 cd $PROJECT_ROOT_DIR
 
-build_safety_viz_image() {
-  cd ../safety_viz
-  docker build . -t safety_viz
-  cd -
-}
-
-
-
 restart_application() {
-  docker pull
-  docker-compose down
-  docker-compose up -d
+  docker compose pull
+  docker compose down
+  docker compose up -d
 }
 
 
 echo "INFO: Pulling changes LKPR distribution configuration ..."
 git pull
-echo "INFO: Building safety_viz docker image ..."
-build_safety_viz_image
 echo "INFO: Starting LKPR deployment ..."
 restart_application
 #echo "INFO: Send notification about sucessfull deployment ..."
