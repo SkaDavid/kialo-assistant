a2192f66e16378c98edc4f98e162a9608c2c3a70
SkaDavid <david.skacha@gmail.com>
2026-04-19 12:43:21 +0200

    vite setup

--- web-client/vite.config.js
+++ web-client/vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+    server: {
+      proxy: {
+      '/api': {
+        target: 'http://kialo-assistant:8080', // Interní název služby v Dockeru
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    },
+    watch: {
+      usePolling: true, 
+    },
+    host: true, 
+    strictPort: true,
+    port: 5173,
+  },
 })
