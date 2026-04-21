3eec3685c154a9c9a166d9ba0ffa73b7cf0792ae
SkaDavid <david.skacha@gmail.com>
2026-04-21 15:08:23 +0200

    Implement global exception handler

---
+++ kialo-assistant/src/main/java/cvut/fel/kbss/exception/GlobalExceptionHandler.java
@@ -0,0 +1,36 @@
+package cvut.fel.kbss.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(ThesisNotDefinedException.class)
+    public ResponseEntity<Object> handleThesisNotDefined(ThesisNotDefinedException ex) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("message", "Thesis is empty");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(body);
+    }
+    @ExceptionHandler({APIkeyNotFoundException.class, OpenAINotRespondingException.class})
+    public ResponseEntity<Object> handleThesisNotDefined(Exception ex) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("message", "AI features are not available at the moment");
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(body);
+    }
+
+    @ExceptionHandler({UserNotFoundException.class, DebateNotFoundException.class})
+    public ResponseEntity<Object> handleNotFound(Exception ex) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("message", "The content you are looking for was not found");
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(body);
+    }
+}
